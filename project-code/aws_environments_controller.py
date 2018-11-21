62bc68389d0f098a25ebe6b1483a77a88b62c4ec
Ian Sims <isims@iu.edu>
2018-11-21 13:57:35 -0600

    Update aws_environments_controller.py

--- project-code/aws_environments_controller.py
+++ project-code/aws_environments_controller.py
@@ -1,18 +1,13 @@
 import connexion
 import six
+import subprocess
 
 from swagger_server import util
 
 
 def create_emr():  # noqa: E501
-    """Create EMR Cluster
-
-     # noqa: E501
-
-
-    :rtype: str
-    """
-    return 'do some magic!'
+    cluster_id=subprocess.run("aws emr create-cluster --name='MyJupyterHubCluster' --release-label emr-5.19.0 --applications Name=JupyterHub --log-uri s3://sims-analysis-bucket/MyJupyterClusterLogs --use-default-roles --instance-type m4.large --instance-count 2 --ec2-attributes KeyName=win-ec2-key")
+    return cluster_id
 
 
 def delete_emr(cluster_id):  # noqa: E501
