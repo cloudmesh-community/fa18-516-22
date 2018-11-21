ae379759d85e0749a1ad4926671948bed48b470e
Ian Sims <isims@iu.edu>
2018-11-21 13:43:47 -0600

    Create aws_environments_controller.py

---
+++ project-code/aws_environments_controller.py
@@ -0,0 +1,41 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def create_emr():  # noqa: E501
+    """Create EMR Cluster
+
+     # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'do some magic!'
+
+
+def delete_emr(cluster_id):  # noqa: E501
+    """Terminate EMR Cluster
+
+     # noqa: E501
+
+    :param cluster_id: 
+    :type cluster_id: str
+
+    :rtype: str
+    """
+    return 'do some magic!'
+
+
+def get_status_emr(cluster_id):  # noqa: E501
+    """Get the status of EMR cluster
+
+     # noqa: E501
+
+    :param cluster_id: 
+    :type cluster_id: str
+
+    :rtype: str
+    """
+    return 'do some magic!'
