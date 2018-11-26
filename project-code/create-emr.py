afc2b16d98468e906419864d127167ce11017ef7
Ian Sims <isims@iu.edu>
2018-11-25 23:47:31 -0600

    Update create-emr.py

--- project-code/create-emr.py
+++ project-code/create-emr.py
@@ -4,16 +4,21 @@ import json
 def create_emr():
    c_id = subprocess.run("aws emr create-cluster --name='E516-JupyterHub-Cluster' --release-label emr-5.19.0 --applications Name=JupyterHub --log-uri s3://e516-jupyterhub-backup/JupyterClusterLogs --use-default-roles --ec2-attributes SubnetIds=subnet-d0169eaa,KeyName=dlec2-key,AdditionalMasterSecurityGroups=['sg-01c1d97ca12d1f2e7'] --instance-count 2 --instance-type m4.large --configurations file://E516-Jupyter-Config.json --output text", shell=True, stdout=subprocess.PIPE)
    cid = c_id.stdout.decode('utf-8')
-   p_dns = subprocess.run("aws emr describe-cluster --cluster-id " + cid + " --query 'Cluster.MasterPublicDnsName' --output text", shell=True, stdout=subprocess.PIPE)
-   pdns = p_dns.stdout.decode('utf-8')
-   
-   rtn_dict =	{
-     "ClusterId": cid,
-     "CheckClusterStatus": ("http://ec2-18-191-50-79.us-east-2.compute.amazonaws.com:8081/emr/get/?" + cid),
-     "JupyterHub": ("https://" + pdns + ":9443"),
+   cid = cid.rstrip()
+   time.sleep(2)
+
+   client = boto3.client('emr')
+   pdns = client.describe_cluster(ClusterId=cid)
+
+   response = client.list_clusters(ClusterStates=['STARTING', 'BOOTSTRAPPING', 'RUNNING', 'WAITING'])
+
+   rtn_dict =   {
+     #"ClusterId": cid,
+     #"CheckClusterStatus": ("http://ec2-18-191-50-79.us-east-2.compute.amazonaws.com:8081/emr/get/?" + cid),
+     #"JupyterHub": ("https://" + response + ":9443"),
      "JupyterUN": "jovyan",
      "JupyterPW": "jupyter"
    }
-   return rtn_dict
+   return pdns
 
 print(create_emr())
