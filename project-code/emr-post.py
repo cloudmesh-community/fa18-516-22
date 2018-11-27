1d631a808c0ef187a3f3e1fd75475843139ec8ef
Ian Sims <isims@iu.edu>
2018-11-26 20:03:45 -0600

    Update emr-post.py

--- project-code/emr-post.py
+++ project-code/emr-post.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def create_emr():
+def emr_post():
    
    c_id = subprocess.run("aws emr create-cluster --name='E516-JupyterHub-Cluster' --release-label emr-5.19.0 --applications Name=JupyterHub --log-uri s3://e516-jupyterhub-backup/JupyterClusterLogs --use-default-roles --ec2-attributes SubnetIds=subnet-d0169eaa,KeyName=dlec2-key,AdditionalMasterSecurityGroups=['sg-01c1d97ca12d1f2e7'] --instance-count 2 --instance-type m4.large --configurations file://E516-Jupyter-Config.json --output text", shell=True, stdout=subprocess.PIPE)
    cid = c_id.stdout.decode('utf-8')
