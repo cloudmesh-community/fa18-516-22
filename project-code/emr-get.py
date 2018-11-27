cdce5b95f55e7bef5d9d37f64c855e84e5e82538
Ian Sims <isims@iu.edu>
2018-11-26 18:30:29 -0600

    Create emr-get.py

---
+++ project-code/emr-get.py
@@ -0,0 +1,12 @@
+import boto3
+
+client = boto3.client('emr')
+pdns = client.describe_cluster(ClusterId=cid)
+
+rtn_dict =   {
+     #"ClusterId": cid,
+     #"CheckClusterStatus": ("http://ec2-18-191-50-79.us-east-2.compute.amazonaws.com:8081/emr/get/?" + cid),
+     #"JupyterHub": ("https://" + response + ":9443"),
+     "JupyterUN": "jovyan",
+     "JupyterPW": "jupyter"
+   }
