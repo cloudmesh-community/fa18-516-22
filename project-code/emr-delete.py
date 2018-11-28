1869c637358773caafe5ea65ad0d29e7d04c1f80
Ian Sims <isims@iu.edu>
2018-11-27 20:13:25 -0600

    Update emr-delete.py

--- project-code/emr-delete.py
+++ project-code/emr-delete.py
@@ -6,7 +6,7 @@ def emr_delete(cid):
      rtn_dict =   {
          "ClusterId": cid,
          "Status": "TERMINATING"
+         "CheckClusterStatus": ("http://ec2-18-191-50-79.us-east-2.compute.amazonaws.com:8080/api/emr/" + cid)
      }
      return rtn_dict
-    
-    
+      
