ae9ac8a7ff3ded464ab5b3f31cff64e8c31537a8
Ian Sims <isims@iu.edu>
2018-11-27 21:31:31 -0600

    Update emr-delete.py

--- project-code/emr-delete.py
+++ project-code/emr-delete.py
@@ -3,9 +3,9 @@ import subprocess
 def emr_delete(cid):
 
      subprocess.run("aws emr terminate-clusters --cluster-ids " + cid, shell=True)
-     
+
      c_status = ("http://ec2-18-191-50-79.us-east-2.compute.amazonaws.com:8080/api/emr/info/" + cid)
-     
+
      rtn_dict =   {
          "ClusterId": cid,
          "Status": "TERMINATING",
