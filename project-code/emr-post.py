e4f66312f1c7efa3ee85e79a66d6a2dd627fd143
Ian Sims <isims@iu.edu>
2018-11-27 20:51:33 -0600

    Update emr-post.py

--- project-code/emr-post.py
+++ project-code/emr-post.py
@@ -2,17 +2,15 @@ import subprocess
 
 def emr_post(num_nodes):
 
-   aws_cmd = "aws emr create-cluster --name='E516-JupyterHub-Cluster'"
+   aws_cmd = "aws emr create-cluster --name=\'E516-JupyterHub-Cluster\'"
    aws_cmd = aws_cmd + " --release-label emr-5.19.0"
    aws_cmd = aws_cmd + " --applications Name=JupyterHub"
    aws_cmd = aws_cmd + " --log-uri s3://e516-jupyterhub-backup/JupyterClusterLogs"
    aws_cmd = aws_cmd + " --use-default-roles"
-   aws_cmd = aws_cmd + " --ec2-attributes SubnetIds=subnet-d0169eaa,KeyName=dlec2-key,AdditionalMasterSecurityGroups=['sg-01c1d97ca12d1f2e7']"
+   aws_cmd = aws_cmd + " --ec2-attributes SubnetIds=subnet-d0169eaa,KeyName=dlec2-key,AdditionalMasterSecurityGroups=[\'sg-01c1d97ca12d1f2e7\']"
    aws_cmd = aws_cmd + " --instance-count " + str(num_nodes)
    aws_cmd = aws_cmd + " --instance-type m4.large"
-   aws_cmd = aws_cmd + " --configurations '" + "[{\"Classification\":\"jupyter-s3-conf\",\""
-   aws_cmd = aws_cmd + "\"Properties\":{\"s3.persistence.bucket\":\"e516-jupyter-backup\","
-   aws_cmd = aws_cmd + "\"s3.persistence.enabled\":\"true\"},\"Configurations\":[]}]\'\""
+   aws_cmd = aws_cmd + " --configurations \'[{\"Classification\":\"jupyter-s3-conf\",\"Properties\":{\"s3.persistence.bucket\":\"e516-jupyter-backup\",\"s3.persistence.enabled\":\"true\"},\"Configurations\":[]}]\'"
    aws_cmd = aws_cmd + " --output text"
 
    c_id = subprocess.run(aws_cmd, shell=True, stdout=subprocess.PIPE)
