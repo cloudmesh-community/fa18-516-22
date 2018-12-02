c5ff59b45df9bbcd97386ad98b4169bcae8d928e
Ian Sims <isims@iu.edu>
2018-12-01 21:20:24 -0600

    Update s3-get.py

--- project-code/s3-get.py
+++ project-code/s3-get.py
@@ -3,11 +3,11 @@ import boto3
 def search_s3_by_ext(bucket, path, extension):
 
     client = boto3.client('s3')
-    obj = client.list_objects_v2(Bucket=mybucket, StartAfter=path )
+    obj = client.list_objects_v2(Bucket=bucket, StartAfter=path )
 
     rtn_list = []
     for object in obj['Contents']:
-        if object['Key'][-(len(file_extension)):] == file_extension:
+        if object['Key'][-(len(extension)):] == extension:
             rtn_list.append(object['Key'])
 
     return(rtn_list)
